373f64ca172dbc2a2ee1024f7fb1e72799ec6cca
deepak <patel.deepak750@gmail.com>
2022-01-19 18:45:46 +0530

    added all the functionlaities

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ let access_token = "9204632f";
 // const btn = document.getElementById("btn");
 const search = document.getElementById("search");
 const suggestion = document.getElementById("match-list");
+let currentMovie = {};
+// this function will add the clicked movie into the favourite list in local staorage 
+function favMovie(e) {
+  const first = e.target.name.split(" ");
+  const movieName = first[0] + first[1];
+  console.log("details", movieName);
+  if (e.target.innerHTML == "Favourite") {
+    e.target.innerHTML = "Unfavourite";
+    let favmovie = JSON.parse(localStorage.getItem("favMovie")) || [];
+    let results = JSON.parse(localStorage.getItem("results")) || [];
+    favmovie.push(results[Number(e.target.id)]);
+    localStorage.setItem("favMovie", JSON.stringify(favmovie));
+    e.target.value = " ";
+    // var div = this.parentElement;
+    // div.style.display = "none";
+  }
+}
 
+function movieDetails(event) {
+  let results = JSON.parse(localStorage.getItem("results")) || [];
+  let current_movie = results[Number(event.target.id)];
+  localStorage.setItem("current_movie", JSON.stringify(current_movie));
+  window.location.assign("movie.html");
+}
+// this will fetch the api of words typed by the user whenever it enters the words in input tag
 search.addEventListener("input", (e) => {
   const fetchApi = async function () {
     //  await fetch(`http://www.omdbapi.com/?t=${e.target.value}&apikey=${access_token}`)
@@ -19,66 +43,26 @@ search.addEventListener("input", (e) => {
     const data = await response.json();
     console.log(data);
     //  Object.entries(data).forEach(v => {
-
+    let results = JSON.parse(localStorage.getItem("results")) || [];
+    results.push(data);
+    localStorage.setItem("results", JSON.stringify(results));
     const avatar = data.Title;
     const imgsrc = data.Poster;
+    currentMovie = data;
     suggestion.innerHTML += `
           <div class="card-body">
         <h5 class="card-title">${avatar}</h5>
-        <button id="fav" on>Favourite</button>
-        <button id="detail">Details</button>
+        <img src="${imgsrc}">
+        <button class="btn btn-primary" id="${results.length - 1}" name=${JSON.stringify(
+      data
+    )} onclick="favMovie(event)">Favourite</button>
+        <button class="btn btn-primary" id="${
+          results.length - 1
+        }" onclick=movieDetails(event)>Details</button>
       </div>`;
-      let favmovie = JSON.parse(localStorage.getItem('favMovie')) || [];
-      localStorage.getItem('favMovie');
-       
-    let favlist = document.querySelectorAll('#fav');
-    favlist.forEach((fav)=>{
-       
-    fav.addEventListener("click", function () {
-      if(fav.innerHTML == "Favourite"){
-        fav.innerHTML = "Unfavourite";
-        favmovie.push(data);
-        localStorage.setItem('favMovie', JSON.stringify(favmovie));
-        e.target.value = " ";
-        // var div = this.parentElement;
-        // div.style.display = "none";
-       
-      }
-     
-    });
-    })
 
-    
-    
-    // let detail = document.getElementById('detail');
-    // let moviedetail = document.getElementById('movie-detail');
-    // console.log(moviedetail);
-    // detail.addEventListener('click', function(){
-      
-      
-    //   moviedetail.innerHTML += `<div class="card" style="width: 18rem;">
-    //   <img src="${imgsrc}" class="card-img-top" alt="...">
-    //   <div class="card-body">
-    //     <h5 class="card-title">${avatar}</h5>
-    //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //   </div>
-    //   <ul class="list-group list-group-flush">
-    //     <li class="list-group-item">An item</li>
-    //     <li class="list-group-item">A second item</li>
-    //     <li class="list-group-item">A third item</li>
-    //   </ul>
-    //   <div class="card-body">
-    //     <a href="#" class="card-link">Card link</a>
-    //     <a href="#" class="card-link">Another link</a>
-    //   </div>
-    // </div>`
-    
-     
-    // })
-    //  });
+   
   };
 
   fetchApi();
-  
 });
-
